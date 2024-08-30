dc1e1712fb18b51b68b04f9704c8e46999d1db5e
Eliott Reyes <eliottreyes22@gmail.com>
2024-08-30 08:56:50 -0400

    a

--- MiniNetflix.Core.Application/Features/Movies/Command/Create/CreateMovieCommandValidator.cs
+++ MiniNetflix.Core.Application/Features/Movies/Command/Create/CreateMovieCommandValidator.cs
@@ -11,20 +11,20 @@ namespace MiniNetflix.Core.Application.Features.Movies.Command.Create
                 .NotNull().WithMessage("El nombre de la pelicula no puede ser nulo");
 
             RuleFor(m => m.createMovieDTO.CoverImage)
-                .NotEmpty().WithMessage("El nombre de la pelicula no puede estar vacío")
-                .NotNull().WithMessage("El nombre de la pelicula no puede ser nulo");
+                .NotEmpty().WithMessage("La imagen de la pelicula no puede estar vacío")
+                .NotNull().WithMessage("La imagen de la pelicula no puede ser nulo");
 
             RuleFor(m => m.createMovieDTO.MovieLink)
-                .NotEmpty().WithMessage("El nombre de la pelicula no puede estar vacío")
-                .NotNull().WithMessage("El nombre de la pelicula no puede ser nulo");
+                .NotEmpty().WithMessage("El enlace de la pelicula no puede estar vacío")
+                .NotNull().WithMessage("El enlace de la pelicula no puede ser nulo");
 
             RuleFor(m => m.createMovieDTO.ProducerId)
-                .NotEmpty().WithMessage("El nombre de la pelicula no puede estar vacío")
-                .NotNull().WithMessage("El nombre de la pelicula no puede ser nulo");
+                .NotEmpty().WithMessage("Tiene que indicar una productora a la pelicula, no puede estar vacía")
+                .NotNull().WithMessage("La productora de la pelicula no puede ser nulo");
 
             RuleFor(m => m.createMovieDTO.MovieGenres)
-                .NotEmpty().WithMessage("El nombre de la pelicula no puede estar vacío")
-                .NotNull().WithMessage("El nombre de la pelicula no puede ser nulo");
+                .NotEmpty().WithMessage("Tiene que indicar uno o mas generos a la pelicula, no puede estar vacío")
+                .NotNull().WithMessage("Los generos de la pelicula no pueden ser nulos");
 
         }
     }
