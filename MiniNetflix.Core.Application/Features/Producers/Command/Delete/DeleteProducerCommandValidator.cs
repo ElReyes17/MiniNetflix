f9600559825b9ded19f3d9347bcd250794b5f559
Eliott Reyes <eliottreyes22@gmail.com>
2024-09-03 16:24:45 -0400

    uy

--- MiniNetflix.Core.Application/Features/Producers/Command/Delete/DeleteProducerCommandValidator.cs
+++ MiniNetflix.Core.Application/Features/Producers/Command/Delete/DeleteProducerCommandValidator.cs
@@ -9,7 +9,12 @@ namespace MiniNetflix.Core.Application.Features.Producers.Command.Delete
     {
         public DeleteProducerCommandValidator(IProducerRepository producerRepository)
         {
-            
+            RuleFor(m => m.Id)
+                .NotEmpty().WithMessage("El Id no puede estar vacio")
+                .NotNull().WithMessage("El Id no puede Ser nulo")
+                .MustAsync(async (id, cancellationToken) =>
+                !await producerRepository.isExist(id)).WithMessage("El Id Ingresado no existe")
+                .WithName("Id de la Productora");
         }
     }
 }
